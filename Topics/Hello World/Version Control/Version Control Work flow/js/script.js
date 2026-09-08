a59494c1e2c36d77038b041cd97ebb5df4a5ade8
AlyciaMal17 <alyciamalonga@gmail.com>
2026-09-08 17:11:39 -0400

    I changed the color to blue and made the circle wider

--- Topics/Hello World/Version Control/Version Control Work flow/js/script.js
+++ Topics/Hello World/Version Control/Version Control Work flow/js/script.js
@@ -42,7 +42,7 @@ function draw() {
         // No line around the levels
         noStroke();
         // Set the fill colour to our shade (RGB)
-        fill(shade, shade, 850);
+        fill(shade, shade, 750);
         // Draw rectangles from the centre
         rectMode(CENTER);
         // Draw the rectangle in the centre of the canvas
