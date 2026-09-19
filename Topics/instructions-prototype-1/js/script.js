c467ed7a12c354af8c39e872a62047826bc50a20
AlyciaMal17 <alyciamalonga@gmail.com>
2026-09-19 17:28:35 -0400

    trial shape of prototype 1

--- Topics/instructions-prototype-1/js/script.js
+++ Topics/instructions-prototype-1/js/script.js
@@ -1,24 +1,20 @@
 /**
- * Title of Project
- * Author Name
+ * Prototype Project 1
+ * Alycia Malonga
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Working on the three prototype projects for the first assignment in the course.
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
 
+function setup() {
+// Create a canvas to draw on
+createCanvas(640, 480);
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+// Set the background color
+background(255, 100, 100);
+rect(200, 80, 240, 320);
 }
