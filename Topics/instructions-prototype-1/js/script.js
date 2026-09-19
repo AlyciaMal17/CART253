7f75683f50379d19551af82db98549412243b47e
AlyciaMal17 <alyciamalonga@gmail.com>
2026-09-19 17:36:40 -0400

    added description for the steps of my project

--- Topics/instructions-prototype-1/js/script.js
+++ Topics/instructions-prototype-1/js/script.js
@@ -3,18 +3,25 @@
  * Alycia Malonga
  * 
  * Working on the three prototype projects for the first assignment in the course.
+ * Prototype 1 example;
  */
 
 "use strict";
 
-
+/**
+ * creating the canvas for the prototype
+ */
 function setup() {
 // Create a canvas to draw on
 createCanvas(640, 480);
 }
 
+/**
+ * Draws the background and needed shape for the project example 1
+ */
 function draw() {
 // Set the background color
 background(255, 100, 100);
+// Draw a rectangle in the center of the canva
 rect(200, 80, 240, 320);
 }
