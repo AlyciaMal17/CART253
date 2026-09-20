84e5881543064ecc99a049e7a236d245b71728ea
AlyciaMal17 <alyciamalonga@gmail.com>
2026-09-19 21:09:51 -0400

    resaved the folder because of error

---
+++ Topics/instructions-prototype-2/README.md
@@ -0,0 +1,22 @@
+# Prototype Project #2
+
+Alycia Malonga
+
+[View this project online](https://editor.p5js.org/AlyMal17/sketches/EOGSz0UR0)
+
+## Description
+
+This is just a static drawing of an ice cream cone with three scoops, made out of basic p5 shapes like triangles, circles, arcs. Each scoop has a little smiley face, and there's a scalloped border between the cone and the scoops. Nothing to click or interact with, just meant to be a fun, cartoony image to look at.
+
+## Attribution
+
+This bit should attribute any code, assets or other elements used taken from other sources. For example:
+
+> - This project uses [p5.js](https://p5js.org).
+
+
+## License
+
+This bit could include the license you want to apply to your work. For example:
+
+> This project is licensed under a Creative Commons Attribution ([CC BY 4.0](https://creativecommons.org/licenses/by/4.0/deed.en)) license with the exception of libraries and other components with their own licenses.
---
+++ Topics/instructions-prototype-2/js/script.js
@@ -0,0 +1,125 @@
+/**
+ * Prototype Project 2
+ * Alycia Malonga
+ * 
+ * Working on two of the three prototype projects for the first assignment in the course.
+ * Prototype 2 example; The Ice Cream
+ */
+
+"use strict";
+
+/**
+ * Creating the canvas and setting up the environment for the project.
+*/
+function setup() {
+// Create a canvas to draw on
+createCanvas(640, 640);
+}
+
+/**
+ * Draws the background and needed shape for the project example 1
+ */
+function draw() {
+// Set the background color to a blue sky color
+background(252, 179, 245);
+
+
+
+// Bottom scoop
+push();
+//  Set the fill color to a pinkish color for the bottom scoop of ice cream
+fill(255, 102, 178);
+ellipse(320, 300, 140, 140);
+pop();
+
+// Bottom scoop eyes and smile
+push();
+// Set the fill color to black for the eyes
+fill(0);
+// Draw the eyes
+ellipse(295, 285, 10, 10); // left eye
+ellipse(345, 285, 10, 10); // right eye
+noFill();
+// Draw the smile
+stroke(0);
+// Set the stroke weight for the smile
+strokeWeight(3);
+// Draw the smile using an arc
+arc(320, 300, 40, 30, 0, PI); // smile
+pop();
+
+
+
+// Middle scoop
+push();
+// Set the fill color to a light pink for the middle scoop of ice cream
+fill(255, 153, 200);
+// Draw the middle scoop of ice cream
+ellipse(320, 190, 140, 140);
+pop();
+// Middle scoop eyes and smile
+push();
+// Set the fill color to black for the eyes
+fill(0);
+// Draw the eyes
+ellipse(295, 175, 10, 10);
+ellipse(345, 175, 10, 10);
+noFill();
+// Draw the smile
+stroke(0);
+strokeWeight(3);
+arc(320, 190, 40, 30, 0, PI);
+pop();
+
+
+
+// Top scoop
+push();
+// Set the fill color to a light pink for the top scoop of ice cream
+fill(255, 200, 220);
+//  Draw the top scoop of ice cream
+ellipse(320, 80, 140, 140);
+pop();
+push();
+// Set the fill color to black for the eyes
+fill(0);
+// Draw the eyes
+ellipse(295, 65, 10, 10);
+ellipse(345, 65, 10, 10);
+// Draw the smile
+noFill();
+// Set the stroke color and weight for the smile
+stroke(0);
+// Set the stroke weight for the smile
+strokeWeight(3);
+arc(320, 80, 40, 30, 0, PI);
+pop();
+
+
+push();
+// Set the fill color to a light brown for the ice cream cone
+fill(255, 204, 153);
+// Draw the ice cream cone
+triangle(320, 567, 240, 367, 400, 367);
+pop();
+
+push();
+noStroke();
+fill(150, 90, 40); 
+// darker brown band
+rect(240, 360, 160, 15); 
+pop();
+
+push();
+noStroke();
+fill(255, 255, 255); 
+// white scallops, change if you want a different color
+for (let x = 250; x <= 390; x += 20) {
+  arc(x, 367, 20, 20, 0, PI); 
+// half circle bumps
+}
+pop();
+
+
+
+}
