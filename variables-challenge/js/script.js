4bd82e97cfde500ebd17adc56ca299dcfa0adf3d
AlyciaMal17 <alyciamalonga@gmail.com>
2026-09-22 16:18:29 -0400

    making mr furious even angrier

--- variables-challenge/js/script.js
+++ variables-challenge/js/script.js
@@ -1,24 +1,84 @@
 /**
- * Title of Project
- * Author Name
+ * Variables Challenge
+ * Alycia Malonga and Mariam-Choukri Mahdi
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ *  A guy who becomes visibly furious!
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+x : 200,
+y : 200,
+size: 100,
 
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225,
+  }
+};
+
+let sky = {
+    // color of the sky
+    fill: {
+        r: 160,
+        g: 180,
+        b: 200,
+    }
 }
 
+let bird = {
+    // Position 
+  x: 10,
+  y: 200
+  
+}
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
+
+/**
+ * Draw (and update) Mr. Furious
+ */
 function draw() {
+background(sky.fill.r, sky.fill.g, sky.fill.b);
+
+// Update Mr. Furious's colour to be more red over time
+mrFurious.fill.g = mrFurious.fill.g - 1;
+mrFurious.fill.b = mrFurious.fill.b - 1;
+
+// Update sky color to be more darker over time
+sky.fill.r = sky.fill.r - 10;
+sky.fill.g = sky.fill.g - 10;
+sky.fill.b = sky.fill.b - 10;
+
+//Move the bird
+bird.x = bird.x + 3;
+
+//shake his head
+mrFurious.x = mrFurious.x + random(-2,2);
+mrFurious.y = mrFurious.y - random(-2,2);
+
+
+// Draw Mr. Furious as a coloured circle
+push();
+noStroke();
+fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+pop();
+
+// added the annoying bird
+push();
+fill (255,20,147)
+circle (bird.x,bird.y,25)
+
 
 }
